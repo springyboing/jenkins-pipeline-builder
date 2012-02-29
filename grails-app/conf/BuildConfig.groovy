d351e0c60ac6f73a9a7c6602c6aefcdb542533c6
Tim Redding <tim@accio.co.uk>
2012-02-29 08:10:18 +0000

    Jenkins-cli.jar now resolves by dependancy resolution

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,6 +17,7 @@ grails.project.dependency.resolution = {
         // from public Maven repositories
         mavenCentral()
         //mavenLocal()
+	mavenRepo "http://maven.jenkins-ci.org/service/local/repositories/releases/content/"
         mavenRepo "http://snapshots.repository.codehaus.org"
         mavenRepo "http://repository.codehaus.org"
         mavenRepo "http://download.java.net/maven/2/"
@@ -26,7 +27,9 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.5'
-	build "org.jenkins-ci.main:cli:latest.integration"
+	//build "org.jenkins-ci.main:cli:latest.integration"
+	build "org.jenkins-ci.main:cli:1.452"
+
     }
 
     plugins {
