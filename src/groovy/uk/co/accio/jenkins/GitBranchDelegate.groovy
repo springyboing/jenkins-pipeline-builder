62067054503000128b086e0901afbdd1a06792d3
Tim Redding <tim@accio.co.uk>
2012-03-08 01:54:54 +0000

    Continued to extend my dsl around the Git config area

---
+++ src/groovy/uk/co/accio/jenkins/GitBranchDelegate.groovy
@@ -0,0 +1,27 @@
+package uk.co.accio.jenkins
+
+/*
+<hudson.plugins.git.BranchSpec>
+<name>master</name>
+</hudson.plugins.git.BranchSpec>
+
+ */
+class GitBranchDelegate implements Buildable {
+
+    String _topLevelElement = "hudson.plugins.git.BranchSpec"
+    String _name
+    
+    void name(name) {
+        this._name = name
+    }
+
+    def void build(GroovyObject builder) {
+        def obj = {
+            "${_topLevelElement}"() {
+                name(_name, [:])
+            }
+        }
+        obj.delegate = builder
+        obj()
+    }
+}
---
+++ test/unit/uk/co/accio/jenkins/bpl/ConfigFileSpec.groovy
@@ -0,0 +1,65 @@
+package uk.co.accio.jenkins.bpl
+
+import grails.plugin.spock.*
+import org.custommonkey.xmlunit.*
+
+class ConfigFileSpec extends UnitSpec {
+
+    def "compair simplest config file possible"() {
+        setup:
+            String expectedXml = loadFile('config-minimal.xml')
+            String personAsXml = """<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class="hudson.scm.NullSCM"/>
+  <canRoam>false</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers class="vector"/>
+  <concurrentBuild>false</concurrentBuild>
+  <builders/>
+  <publishers/>
+  <buildWrappers/>
+</project>"""
+
+        when:
+            def xmlDiff = new Diff(personAsXml, expectedXml)
+
+        then:
+            xmlDiff.identical()
+            //xmlDiff.similar()
+    }
+
+    def "create simplest config file possible"() {
+        setup:
+            String expectedXml = loadFile('config-minimal.xml')
+            String dsl = """build {
+                                name "Billy"
+                                desc "My First Desc"
+                            }
+                         """
+            String generatedXml = ''//uk.co.accio.jenkins.BuildConfiguration.runJenkinsBuilder(dsl)
+
+        when:
+            def xmlDiff = new Diff(generatedXml, expectedXml)
+
+        then:
+            xmlDiff.identical()
+            //xmlDiff.similar()
+    }
+
+    String loadFile(name) {
+        def value
+        def inputStream
+        try {
+            inputStream = getClass().getClassLoader().getResourceAsStream(name)
+            value = inputStream.text
+        } finally {
+            org.apache.commons.io.IOUtils.closeQuietly(inputStream)
+        }
+        return value
+    }
+}
